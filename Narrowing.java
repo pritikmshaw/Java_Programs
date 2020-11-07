dfe068f23bd63111d93c6ae201bf8492a9ba23f8
pritikmshaw <pritikmshaw18@gmail.com>
2020-11-07 21:19:55 +0530

    type casting 2.Narrowing

---
+++ Narrowing.java
@@ -0,0 +1,9 @@
+public class Narrowing { // type casting 
+  public static void main(String[] args) {
+    double myDouble = 9.78;
+    int myInt = (int) myDouble; // Manual casting: double to int
+
+    System.out.println(myDouble);   // Outputs 9.78
+    System.out.println(myInt);      // Outputs 9
+  }
+}
