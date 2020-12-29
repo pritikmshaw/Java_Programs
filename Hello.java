48edc1a49591c9e97c982f088bf5e97fc26304d3
pritikmshaw <pritikmshaw18@gmail.com>
2020-12-29 22:11:20 +0530

    hello world static and non static method

--- Hello.java
+++ Hello.java
@@ -1,5 +1,17 @@
+import java.util.*;
 public class Hello  {
+    public void nonStaticMethod() {
+        System.out.println("Non Static Method");
+    }
+
+    public static void staticmMthod() {
+        System.out.println("Static method");
+    }
     public static void main (String args[]) {
         System.out.println("Hello World");
+        HelloWorld helloWorldObject = new HelloWorld();
+        helloWorldObject.nonStaticMethod();
+        staticmMthod();
+
     }
 }
