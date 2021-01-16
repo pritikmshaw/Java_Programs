c1cea5785095b5a3b54fa1027553ff1fe535b51b
pritikmshaw <pritikmshaw18@gmail.com>
2021-01-16 14:50:55 +0530

    largest of 3 nos using comand line  error is there

---
+++ LargestNoCommandLine.java
@@ -0,0 +1,17 @@
+// largest of 3 nos using comand line 
+public class LargestNoCommandLine {
+    public static void main (String[] args) {
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+        if(a>b && a>c) {
+            System.out.println("a is " +a+ " greatest");
+        }
+        else if (b>a && b>c) {
+            System.out.println("b is " +b+ " greatest");
+        }
+        else {
+            System.out.println("c is " +c+ " greatest");
+        }
+    }
+}
