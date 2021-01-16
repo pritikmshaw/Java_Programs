ac65264c404f454fe4e5afc373872aadf582963e
pritikmshaw <pritikmshaw18@gmail.com>
2021-01-16 09:56:33 +0530

    Radius of circle

---
+++ Radius.java
@@ -0,0 +1,9 @@
+// Radius of a circle when r is 5
+public class Radius{
+    public static void main (String[] args) {
+        int r= 5;
+        double pi = 3.14 ; 
+        double area = pi * r * r ;
+        System.out.println(" araea of circle : " +area);
+    }
+}
