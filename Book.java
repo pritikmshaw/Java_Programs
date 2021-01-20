b0e97eb94b0dcee8e91a12ea895a573c4a0f1e05
pritikmshaw <pritikmshaw18@gmail.com>
2021-01-20 20:33:18 +0530

    // create class book and create 3 objects of it

---
+++ Book.java
@@ -0,0 +1,19 @@
+// create class book and create 3 objects of it 
+public class Book{
+    public static void main (String[] args) {
+        
+        bookdetails b1 = new bookdetails("Twilight" ,"Stephnimeyer" );
+        bookdetails b2 = new bookdetails("Harry Potter ", "J.K Rowling");
+        System.out.println("title : " + b1.title+ "author: " +b1.author);
+        System.out.println("title : " + b2.title+ "author: " +b2.author);
+    }
+    public static class bookdetails {
+        String title ;
+        String author ;
+        public bookdetails(String title, String author ) {
+            title = " ";
+            author = " " ;
+        }
+
+    }
+}
