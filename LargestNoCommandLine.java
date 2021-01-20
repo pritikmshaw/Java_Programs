05f7d3dc1db4d8f3241618c67f18616aa1d56473
pritikmshaw <pritikmshaw18@gmail.com>
2021-01-20 19:54:37 +0530

    LargestNoCommandLine

--- LargestNoCommandLine.java
+++ LargestNoCommandLine.java
@@ -5,10 +5,10 @@ public class LargestNoCommandLine {
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
         if(a>b && a>c) {
-            System.out.println("a is "+a+" greatest");
+            System.out.println("a is "+a+" greatest ");
         }
         else if (b>a && b>c) {
-            System.out.println("b is " +b+ " greatest");
+            System.out.println("b is " + b+ " greatest");
         }
         else {
             System.out.println("c is " +c+ " greatest");
