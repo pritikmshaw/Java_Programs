34036cbfc1df5d5b8693b1178f2f638b4572b94b
pritikmshaw <pritikmshaw18@gmail.com>
2021-02-14 16:55:56 +0530

    fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -15,11 +15,11 @@ i++;
 }
 }
 public static int fibo (int n) {
-if (n==1)
-return 0;
-else if(n==2)
-return 1;
-else
-return fibo(n-1)+fibo(n-2);
+    if (n==1)
+        return 0;
+    else if(n==2)
+        return 1;
+    else
+        return fibo(n-1)+fibo(n-2);
 }
 }
