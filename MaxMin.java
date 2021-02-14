1c256eb366001315f858bcc7e096623543fce5a5
pritikmshaw <pritikmshaw18@gmail.com>
2021-02-14 16:53:26 +0530

    max min

---
+++ MaxMin.java
@@ -0,0 +1,13 @@
+//Write a Java program to find out the minimumand maximum number from a given array
+//using for each loop.
+import java.util.Scanner;
+public class MaxMin {
+    public static void main (String [] args) {
+        int a [] = [10,20,3,40,50];
+        int max = a[0] ;
+        int min = a[0] ;
+        for(int a : max ) {
+            if(ma)
+        }
+    }
+}
